488b7249b0a32197c3c6e050c42b49375b104569
NatashaProt <natalia.protosov@gmail.com>
2017-04-21 15:39:12 +0300

    litecart_test

---
+++ flitecart/flitecart/LoginTest.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace flitecart
+{
+    [TestFixture]
+    public class MyFirstTest
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new ChromeDriver();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            driver.Url = "http://localhost/litecart/admin/";
+            driver.FindElement(By.Name("username")).SendKeys("admin");
+            driver.FindElement(By.Name("password")).SendKeys("admin");
+         //   driver.FindElement(By.Name("remember_me")).Click();
+            driver.FindElement(By.XPath("//*[@id='box-login']/form/div[2]/button")).Click();
+            wait.Until(ExpectedConditions.TitleIs("My Store"));
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+
+        public object TimeUnit { get; set; }
+    }
+}
